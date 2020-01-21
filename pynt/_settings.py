5e0aee4bdfecfbda732ca53b0c329e902f52a174
Joel Karlsson <joelkarlsson97@gmail.com>
2020-01-21 11:41:37 +0100

    Implement set_fontsize and use_tex

--- pynt/_settings.py
+++ pynt/_settings.py
@@ -1,4 +1,5 @@
 from os import path
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 __all__ = ["set_style", "use_tex", "set_fontsize"]
@@ -6,12 +7,34 @@ _STYLE_DIR = path.abspath(path.join(path.dirname(__file__), 'styles'))
 
 
 def set_style(style="pynt"):
-    plt.style.use(path.join(_STYLE_DIR, style))
+    """
+    Set Pynt style for Pyplot.
+    """
+    try:
+        plt.style.use(path.join(_STYLE_DIR, style))
+    except FileNotFoundError as e:
+        raise ValueError(f"{style} ist not a Pynt style") from e
 
 
-def use_tex():
-    pass
+def use_tex(tex=True, font=None):
+    """
+    Turn on (or off) useage of TeX and set font. If font or tex is False,
+    the font will not be changed.
+    """
+    # if font is None:
+    #     if tex:
+    #         font = {'family': 'serif', 'serif': ['DejaVu Sans']}
+    #     else:
+    #         font = {}
+    # if font ist not False:
+    #     mpl.rc('font', **font)
+    mpl.rc('text', usetex=tex)
 
 
-def set_fontsize():
-    pass
+def set_fontsize(fontsize):
+    """
+    Set Pyplot fontsize.
+    """
+    mpl.rc('font', size=fontsize)
+    mpl.rc('xtick', labelsize=fontsize)
+    mpl.rc('ytick', labelsize=fontsize)
